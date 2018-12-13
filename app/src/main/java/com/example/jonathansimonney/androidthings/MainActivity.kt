3bd8f863eb7474180b8893640d6e55c179a6fe73
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-12-13 10:55:42 +0100

    first test with log.

--- app/src/main/java/com/example/jonathansimonney/androidthings/MainActivity.kt
+++ app/src/main/java/com/example/jonathansimonney/androidthings/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.jonathansimonney.androidthings
 
 import android.app.Activity
 import android.os.Bundle
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
+import com.google.android.things.contrib.driver.button.Button.OnButtonEventListener
+import android.R.attr.button
+import android.util.Log
+import com.google.android.things.contrib.driver.button.Button
+
 
 /**
  * Skeleton of an Android Things activity.
@@ -24,9 +30,18 @@ import android.os.Bundle
  *
  */
 class MainActivity : Activity() {
+    var button = RainbowHat.openButtonC()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnButtonEventListener { button, pressed -> Log.e("buttonPressed", button.toString()) }
     }
+
+    override fun onDestroy() {
+        button.close()
+        super.onDestroy()
+    }
+
 }
